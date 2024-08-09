76c059c043d8ed30c977d59b3393bfdcdf382320
Wamuyukariuki <jaxonkariuki@gmail.com>
2024-08-09 14:56:06 +0300

    Updated income and goals apps

--- income/urls.py
+++ income/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('add/', views.add_income, name='add_income'),
     path('list/', views.income_list, name='income_list'),
+    path('edit/<int:pk>/', views.edit_income, name='edit_income'),
 ]
--- income/views.py
+++ income/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Income
+from django.shortcuts import render, redirect, get_object_or_404
 from .form import IncomeForm
+from .models import Income
 
 
 @login_required
@@ -22,3 +22,20 @@ def add_income(request):
 def income_list(request):
     incomes = Income.objects.filter(user=request.user)
     return render(request, 'income/income_list.html', {'incomes': incomes})
+
+
+@login_required
+def edit_income(request, pk):
+    income = get_object_or_404(Income, pk=pk, user=request.user)
+
+    if request.method == 'POST':
+        form = IncomeForm(request.POST, instance=income)
+        if form.is_valid():
+            form.save()
+            return redirect('income_list')  # Redirect to a relevant page after saving
+        else:
+            print(form.errors)  # Debugging statement to check for form errors
+    else:
+        form = IncomeForm(instance=income)
+
+    return render(request, 'income/edit_income.html', {'form': form})
